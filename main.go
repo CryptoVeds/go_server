b4c10b2704a9dfec1b8eeb03c95b2eade1cc7c9a
VSP108 <ponomarev.vsp@gmail.com>
2022-12-14 13:04:00 +0800

    add echoString function

--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 	"net/http"
 )
 
+func echoString(w http.ResponseWriter, г *http.Request) {
+	fmt.Fprint(w, "Hello, Geekbrains")
+}
+
 func main () { 
-	http.HandleFunc("/", func(w http.ResponseWriter, г *http.Request) {
-		fmt.Fprint(w, "Hello, Geekbrains")
-	})
+	http.HandleFunc("/", echoString)
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, г *http.Request) {
 		fmt.Fprint(w, "OK")
 	})
+
+
 
 	log.Fatal(http.ListenAndServe(":8085", nil))
 }
